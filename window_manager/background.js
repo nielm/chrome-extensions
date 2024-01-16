1c58733f323920780a88bf7e0d5acde60b5c8849
Lukasz Gruszka <lukasz>
2024-01-16 11:44:03 +0100

    Changed screen changed delay to 1s. Added shortcut for rearranging all windows.

--- window_manager/background.js
+++ window_manager/background.js
@@ -13,23 +13,33 @@ async function displaysAsString() {
 
 chrome.commands.onCommand.addListener(async (command) => {
   const shortcutId = parseInt(command.charAt(command.length - 1));
-  if (shortcutId < 1 || shortcutId > 9) {
-    throw new Error(`Invalid command: ${command} - expected id between 1 and 9.`);
+  if (shortcutId < 0 || shortcutId > 9) {
+    throw new Error(`Invalid command: ${command} - expected id between 0 and 9.`);
   }
 
-  const actionsPromise = (await Action.loadAll()).filter(action => action.shortcutId == shortcutId);
-  updateWindowWithActions(await actionsPromise);
+  if (shortcutId == 0) {
+    updateWindows();
+  } else {
+    const actionsPromise = (await Action.loadAll()).filter(action => action.shortcutId == shortcutId);
+    updateWindowWithActions(await actionsPromise);
+  }
 });
 
 chrome.system.display.onDisplayChanged.addListener(async () => {
   const settings = await Settings.load();
   if (settings.triggerOnMonitorChange) {
-    // This event is triggered also on unlock, let's check if anything was really changed.
-    const displays = await displaysAsString();
-    if (currentDisplays != displays) {
-      currentDisplays = displays;
-      setTimeout(updateWindows, 500);
-    }
+    // wait one second before doing anything - until the screens are initialised.
+    setTimeout(
+      async () => {
+        // This event is triggered also on unlock, let's check if anything was really changed.
+        const displays = await displaysAsString();
+        if (currentDisplays != displays) {
+          currentDisplays = displays;
+          updateWindows();
+        }
+      },
+      1000
+    );
   }
 });
 
@@ -40,6 +50,7 @@ chrome.windows.onCreated.addListener(async (window) => {
   }
 });
 
+// This is triggered from the options menu.
 chrome.runtime.onMessage.addListener(
   async function(request, sender, sendResponse) {
     if (request.command === 'updateWindows') {
